86aae55cb8412c3a1ba21f447267e0f042bc54df
leo60228 <iakornfeld@gmail.com>
2019-05-15 13:17:30 -0400

    Allow forwarding linkle args

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,21 @@ fn main() {
     let mut elf = None;
     let mut iargs = args.iter();
 
-    while let Some(arg) = iargs.next() {
-        gcc.arg(&arg);
+    let mut linkle_args: Vec<String> = vec![];
 
+    while let Some(arg) = iargs.next() {
         if arg == "-o" {
+            gcc.arg(&arg);
             output = iargs.next();
             let mut no_nro = output.unwrap().replace(".nro", "");
             no_nro.push_str(".elf");
             gcc.arg(&no_nro);
             elf = Some(no_nro);
+        } else if arg == "--icon-path" || arg == "--romfs-path" || arg == "--nacp-path" {
+            linkle_args.push(arg.to_string());
+            linkle_args.push(iargs.next().unwrap().to_string());
+        } else {
+            gcc.arg(&arg);
         }
     }
 
@@ -31,7 +37,8 @@ fn main() {
     let mut linkle = Command::new("linkle");
     linkle.arg("nro")
         .arg(elf)
-        .arg(output);
+        .arg(output)
+        .args(linkle_args);
     eprintln!("{:?}", linkle);
     assert!(linkle.status().unwrap().success());
 }
